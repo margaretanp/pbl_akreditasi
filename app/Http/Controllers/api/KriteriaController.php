db1b9209b6534f81c27a04b5b63560a5c92eb9ff
margaretanp <margaretanovia1911@gmail.com>
2025-05-23 03:03:43 +0700

    feat: kriteria(migration, seeder, model, controller)

---
+++ app/Http/Controllers/api/KriteriaController.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\storeKriteriaRequest;
+use App\Http\Requests\updateKriteriaRequest;
+use App\Models\KriteriaModel;
+use Illuminate\Http\Request;
+
+class KriteriaController extends Controller
+{
+    public function index()
+    {
+        $kriteria = KriteriaModel::all();
+        return response()->json(
+            [
+                'status' => 'success',
+                'message' => 'Kriteria data retrieved successfully',
+                'data' => $kriteria
+            ],
+            200
+        );
+    }
+
+    public function show($id)
+    {
+        $kriteria = KriteriaModel::find($id);
+
+        if (!$kriteria) {
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'Kriteria not found'
+                ]
+                ,
+                404
+            );
+        }
+
+        return response()->json(
+            [
+                'status' => 'success',
+                'message' => 'Kriteria data retrieved successfully',
+                'data' => $kriteria
+            ]
+            ,
+            200
+        );
+    }
+
+    public function store(storeKriteriaRequest $request)
+    {
+        $data = $request->validated();
+
+        $kriteria = KriteriaModel::create($data);
+
+        return response()->json(
+            [
+                'status' => 'success',
+                'message' => 'Kriteria created successfully',
+                'data' => $kriteria
+            ]
+            ,
+            201
+        );
+    }
+
+    public function update(updateKriteriaRequest $request, $id)
+    {
+        $kriteria = KriteriaModel::find($id);
+
+        if (!$kriteria) {
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'Role not found'
+                ],
+                404
+            );
+        }
+
+        $data = $request->validated();
+
+        $kriteria->update($data);
+
+        return response()->json(
+            [
+                'status' => 'success',
+                'message' => 'Kriteria updated successfully',
+                'data' => $kriteria
+            ],
+            200
+        );
+    }
+
+    public function destroy($id)
+    {
+        $kriteria = KriteriaModel::find($id);
+
+        if(!$kriteria) {
+            return response()->json(
+                [
+                    'status' => 'error',
+                    'message' => 'Kriteria not found'
+                ],
+                404
+            );
+        }
+    
+        $kriteria->delete();
+        return response()->json(
+            [
+                'status' => 'success',
+                'meessage' => 'Role deleted successfully'
+            ],
+            200
+        );
+    }
+}
---
+++ app/Models/KriteriaModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class KriteriaModel extends Model
+{
+    use HasFactory, SoftDeletes;
+    protected $table = 'kriteria';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name'];
+    protected $hidden = ['deleted_at', 'created_at', 'updated_at'];
+    
+}
