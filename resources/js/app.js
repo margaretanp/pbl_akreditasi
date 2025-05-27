3ad2da79ed2fbd26da825c9fbe975b4da0bed894
Johnatan David Gersom <bengbeng9605@gmail.com>
2025-05-27 15:34:15 +0700

    Route Bersama

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,3 +12,10 @@ app.use(router);
 app.use(ToastPlugin);
 
 app.mount("#app");
+// import { createApp } from 'vue';
+// import App from './App.vue';
+// import router from './router';
+
+// createApp(App)
+//     .use(router)
+//     .mount('#app');
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::middleware(['auth', 'verified', 'role:2'])->group(function () {
+// });
+Route::get('/validator/{any?}', function () {
+    return view('app'); // app.blade.php akan load Vue SPA
+})->where('any', '.*');
+
+// Route::middleware(['auth', 'verified'])->group(function () {
+//     Route::prefix('anggota')->middleware(['role:5'])->group(function () {
+//         Route::get('/dashboard', function () {
+//             return view('Home.Dashboard');
+//         });
+//     });
+// });
+
 Route::get('/{any}', function () {
     return view('welcome');  // This returns your index blade file
 })->where('any', '^(?!api).*$');  // This will match all routes except /api
