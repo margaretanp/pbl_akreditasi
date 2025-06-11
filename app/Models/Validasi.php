6000b9fabefeedc37554c534245b3abcf53127c2
margaretanp <margaretanovia1911@gmail.com>
2025-06-11 23:20:13 +0700

    update: kriteria and validasi

--- app/Models/Validasi.php
+++ app/Models/Validasi.php
@@ -13,7 +13,6 @@ class Validasi extends Model
     protected $fillable = ['user_id', 'kriteria_id', 'validated_at', 'komentar', 'is_validated'];
     protected $hidden = ['created_at', 'updated_at', 'deleted_at'];
 
-
     public function user()
     {
         return $this->belongsTo(UsersModel::class, 'user_id', 'id');
